82ba6967246dd00bedcce94104da651d2dda0c18
Jesse Cota <34288347+jaylabs@users.noreply.github.com>
2023-01-15 20:57:15 -0300

    Update app.js

--- app/app.js
+++ app/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = process.env.NODE_PORT || 3000;
 
-app.get("/", (req, res) => res.send("Bye World!"));
+app.get("/", (req, res) => res.send("Bye World!!!"));
 
 app.get("/version", (req, res) => res.send("1.0.0"));
 
