872a6da47313fa47df28c36c5079776de0230929
Jesse Cota <34288347+jaylabs@users.noreply.github.com>
2023-01-15 22:47:24 -0300

    Update app.js

--- app/app.js
+++ app/app.js
@@ -4,7 +4,7 @@ const port = process.env.NODE_PORT || 3000;
 
 app.get("/", (req, res) => res.send("Hello World!"));
 
-app.get("/version", (req, res) => res.send("1.0.2"));
+app.get("/version", (req, res) => res.send("1.0.3"));
 
 app.get("/health/check", (req, res) => res.send("OK"));
 
